ff21b70562c3b86b23d8cc5aa5332070a9a15925
fmarcos38 <fmarcos_23@hotmail.com>
2024-08-28 19:52:55 -0300

    estilos con sass

---
+++ src/components/home/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import styles from './Hero.module.sass'; //importacion de los estilos CSS
+
+
+export const Hero = () => {
+    return(
+      <section className={styles.Hero}>
+        <h1 >Hero</h1>
+      </section>
+    )
+  }
